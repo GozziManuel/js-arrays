90209c647f506d1bad7372c75d273b613864885a
Manuel Gozzi <manuel2006gozzi@gmail.com>
2025-12-18 17:40:49 +0100

    5s request

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -37,6 +37,7 @@ if (index > 0) {
 } else {
   console.log("not present");
 }
-
+console.log("-------------------------");
 // 5. Unisci tutti gli insegnanti nell'array teachers in una stringa  separata da virgole e salvala nella variabile teachersString
-const teachersString = null;
+const teachersString = teachers.toString();
+console.log(teachersString);
